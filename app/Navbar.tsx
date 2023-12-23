ad9584e4a3e0dadd1d53c7765be123ac56144ce6
soha <sohanajmeddin@gmail.com>
2023-12-23 13:01:17 +0330

    initial commit

---
+++ app/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">Logo</Link>
+      <ul className="flex space-x-6 items-center">
+        <li>
+          <Link href="/">Dashboard</Link>
+        </li>
+        <li>
+          <Link href="/issues">Issues</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
